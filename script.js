58b3621aaf2002ad94afbfd625f3c9e7f7c654ec
Sandrine Poissonnet <spoisson@gmail.com>
2021-08-14 10:23:19 -0700

    displayTodos

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ var todosList = document.getElementById('todos-list');
 
 var todos = [];
 
+// Loop through todos and display them
+function displayTodos() {
+  for (var i = 0; i < todos.length; i++) {
+  var li = document.createElement('li');
+  li.innerText = todos[i];
+  todosList.appendChild(li);
+}
+}
+
 function addTodo(event) {
   event.preventDefault();
   var newTodoText = newTodoInput.value;
   todos.push(newTodoText);
   newTodoInput.value = '';
+  displayTodos();
 }
 
 newTodoForm.addEventListener('submit', addTodo);
